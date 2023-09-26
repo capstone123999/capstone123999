d048ec3ea9a4488887eea16785c0691beb2604a5
Jeyi Han <gks20877@naver.com>
2023-09-26 13:58:42 +0900

    j first commit

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teenager_main);
+
+        Button teenagerMainEmergency = findViewById(R.id.teenagerMainEmergency);
+        teenagerMainEmergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Emergency.class);
+                startActivity(intent);
+            }
+        });
+
+        Button teenagerMainNotify = findViewById(R.id.teenagerMainNotify);
+        teenagerMainNotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Notify.class);
+                startActivity(intent);
+            }
+        });
+
+        Button teenagerMainNestFind = findViewById(R.id.teenagerMainNestFind);
+        teenagerMainNestFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FindNest.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton teenagerMainMyPageImage = findViewById(R.id.teenagerMainMyPageImage);
+        teenagerMainMyPageImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyPage.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
