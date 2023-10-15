31143e62ce6acfb36e8441384acc0a6354d2d42e
nsj1201 <nsj1201@naver.com>
2023-10-15 12:41:07 +0900

    20231015 / 12:40

---
+++ app/src/main/java/com/example/capstone/ManagerMain.java
@@ -0,0 +1,51 @@
+package com.example.capstone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManagerMain extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_main);
+
+        Button manager_button1 = findViewById(R.id.manager_button1);
+        Button manager_button2 = findViewById(R.id.manager_button2);
+        Button manager_button3 = findViewById(R.id.manager_button3);
+        Button manager_button4 = findViewById(R.id.manager_button4);
+
+        manager_button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManagerMain.this, ManagerNestSubmitActivity.class);
+                startActivity(intent);
+            }
+        });
+        manager_button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManagerMain.this, ManagerNestMatchResultActivity.class);
+                startActivity(intent);
+            }
+        });
+        manager_button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManagerMain.this, ManagerNotify.class);
+                startActivity(intent);
+            }
+        });
+        manager_button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManagerMain.this, ManagerEmergency.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
