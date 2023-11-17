8bfbe337d3addd9c49066a946700639ec0186a67
nsj1201 <nsj1201@naver.com>
2023-11-17 17:02:34 +0900

    20231117 17:01 일반신고 하드코딩

--- app/src/main/java/com/example/capstone/ManagerNotify.java
+++ app/src/main/java/com/example/capstone/ManagerNotify.java
@@ -1,15 +1,38 @@
 package com.example.capstone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ManagerNotify extends AppCompatActivity {
+
+    Button manager_notify_teenager_button, manager_notify_lender_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_notify);
 
+        manager_notify_teenager_button = findViewById(R.id.manager_notify_teenager_button);
+        manager_notify_lender_button = findViewById(R.id.manager_notify_lender_button);
+
+        manager_notify_teenager_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ManagerNotify.this, ManagerNotifyTeenager.class);
+                startActivity(intent);
+            }
+        });
+
+        manager_notify_lender_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ManagerNotify.this, ManagerNotifyLender.class);
+                startActivity(intent);
+            }
+        });
     }
 }
