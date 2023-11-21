489de609a1bad75863d7fc186ea73ae19f2a869c
nsj1201 <nsj1201@naver.com>
2023-11-21 18:51:11 +0900

    20231121 18:27 마이페이지 회원탈퇴 구현 완료

--- app/src/main/java/com/example/capstone/LenderMyPage.java
+++ app/src/main/java/com/example/capstone/LenderMyPage.java
@@ -1,21 +1,86 @@
 package com.example.capstone;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class LenderMyPage extends AppCompatActivity {
+
+    Button deleteButton,accountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lender_mypage);
+
+
+        deleteButton = findViewById(R.id.deleteButton);
+        accountButton = findViewById(R.id.accountButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LenderMyPage.this);
+                builder.setTitle("탈퇴")
+                        .setMessage("정말로 탈퇴하시겠습니까?")
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // 확인 버튼을 눌렀을 때 수행할 동작
+                                //장채원 작성
+                                // Get SharedPreferences instance
+                                SharedPreferences sharedPreferences = getSharedPreferences("userPrefs", Context.MODE_PRIVATE);
+
+                                // Retrieve ID information using the saved tag
+                                String currentUserId = sharedPreferences.getString("id", null);
+
+                                // Use the received data as needed
+                                if (currentUserId != null) {
+                                    MembershipWithdrawal withdrawal = new MembershipWithdrawal();
+                                    withdrawal.withdrawMembership(currentUserId);
+
+                                    Toast.makeText(LenderMyPage.this, "탈퇴가 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                                    Intent intent2 = new Intent(LenderMyPage.this, MainActivity.class);
+                                    startActivity(intent2);
+                                }else{
+                                    Toast.makeText(LenderMyPage.this, "currentUserId is Null", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // 취소 버튼을 눌렀을 때 수행할 동작
+                                // 원하는 동작을 수행하도록 코드를 추가하세요
+                                Toast.makeText(LenderMyPage.this, "취소 버튼을 눌렀습니다", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+            }
+        });
+
+
+        accountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LenderMyPage.this, AccountInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         ColorStateList iconColor = ContextCompat.getColorStateList(this, R.color.custom_icon_color);
